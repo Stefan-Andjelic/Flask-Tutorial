0852eee164022e2d833be0a48b39bcfb864fdb82
Stefan <stefanandjelic8@gmail.com>
2022-01-26 14:42:24 -0600

    Templates

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,8 +1,5 @@
-from crypt import methods
 import functools
-import imp
-from django.shortcuts import redirect
-from flask import (Blueprint, flash, g, render_template, request, session, url_for)
+from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
 from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
